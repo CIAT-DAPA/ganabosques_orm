68e4b8cc3f7952ada53df5debb4784e01d3424fe
Ramos <J.J.Ramos@cgiar.org>
2025-08-08 08:35:39 -0500

    Added Analysis_id to farmsRisk and EnterpriseRisk collections

--- ganabosques_orm/collections/farmrisk.py
+++ ganabosques_orm/collections/farmrisk.py
@@ -2,11 +2,14 @@ from mongoengine import Document, ObjectIdField, EmbeddedDocumentField, FloatFie
 from ganabosques_orm.auxiliaries.attributes import Attributes
 from ganabosques_orm.collections.farm import Farm
 from ganabosques_orm.collections.farmpolygons import FarmPolygons
+from ganabosques_orm.collections.analysis import Analysis
+
 
 class FarmRisk(Document):
     """Auto-generated MongoDB collection: FarmRisk"""
     meta = {'collection': 'farmrisk'}
     farm_id = ReferenceField(Farm)
+    analysis_id = ReferenceField(Analysis)
     farm_polygons_id = ReferenceField(FarmPolygons)
     deforestation = EmbeddedDocumentField(Attributes)
     protected = EmbeddedDocumentField(Attributes)
