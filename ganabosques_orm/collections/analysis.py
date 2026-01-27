1605dfe304261a8140652a6734ecad827651bf09
CarlosNasayo <carlosnasayov@gmail.com>
2026-01-27 03:19:47 -0500

    added verification, actions, options

--- ganabosques_orm/collections/analysis.py
+++ ganabosques_orm/collections/analysis.py
@@ -1,8 +1,9 @@
-from mongoengine import Document, ObjectIdField, DateTimeField, ReferenceField, EmbeddedDocumentField
+from mongoengine import Document, ObjectIdField, DateTimeField, ReferenceField, EmbeddedDocumentField, EnumField
 from ganabosques_orm.collections.protectedareas import ProtectedAreas
 from ganabosques_orm.collections.farmingareas import FarmingAreas
 from ganabosques_orm.collections.deforestation import Deforestation
 from ganabosques_orm.auxiliaries.typeanalysis import TypeAnalysis
+from ganabosques_orm.enums.valuechain import ValueChain
 
 class Analysis(Document):
     """Auto-generated MongoDB collection: Analysis"""
@@ -13,3 +14,4 @@ class Analysis(Document):
     user_id = ObjectIdField()
     date = DateTimeField()
     type_analysis = EmbeddedDocumentField(TypeAnalysis)
+    value_chain = EnumField(ValueChain)
---
+++ ganabosques_orm/enums/actions.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Actions(Enum):
+    """Auto-generated enum: Actions"""
+    FRONT_ENTERPRISE = "front_enterprise"
+    FRONT_ADM = "front_adm"
+    FRONT_REPORT = "front_report"
+    FRONT_FARMs = "front_farms"
+    API_FARMS="api_farms"
+    API_ENTERPRISE="api_enterprise"
+    API_ADM="api_adm"
