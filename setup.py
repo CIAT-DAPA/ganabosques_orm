f7c7c658103bec7b79717034d647152ee690ccfa
CarlosNasayo <CarlosNasayo@users.noreply.github.com>
2026-02-08 22:23:13 +0000

    Update version to v0.0.31

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ganabosques_orm",
-    version="0.1.0",
+    version="0.0.31",
     description="ORM package for Ganabosques project using MongoEngine",
     author="Steven Sotelo",
     author_email="h.sotelo@cgiar.org",
